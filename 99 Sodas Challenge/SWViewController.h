c4cdb313f17a5603769edbe3b12a60432deeb41c
Shannon Wynn <shannonwynn@mac.com>
2013-10-03 12:42:37 -0400

    Initial Commit

---
+++ 99 Sodas Challenge/SWViewController.h
@@ -0,0 +1,13 @@
+//
+//  SWViewController.h
+//  99 Sodas Challenge
+//
+//  Created by Shannon Wynn on 10/3/13.
+//  Copyright (c) 2013 Shannon Wynn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SWViewController : UIViewController
+
+@end
---
+++ 99 Sodas ChallengeTests/_9_Sodas_ChallengeTests.m
@@ -0,0 +1,34 @@
+//
+//  _9_Sodas_ChallengeTests.m
+//  99 Sodas ChallengeTests
+//
+//  Created by Shannon Wynn on 10/3/13.
+//  Copyright (c) 2013 Shannon Wynn. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface _9_Sodas_ChallengeTests : XCTestCase
+
+@end
+
+@implementation _9_Sodas_ChallengeTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
